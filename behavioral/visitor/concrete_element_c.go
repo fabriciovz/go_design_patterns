986c0e722b5fa6d1056d6534629ef71247cfa3aa
Fabricio Bravo Guevara <fabricio.bravo0@gecgithub01.walmart.com>
2023-11-19 13:53:53 -0300

    adding a new visitor pattern sample

---
+++ behavioral/visitor/concrete_element_c.go
@@ -0,0 +1,14 @@
+package visitor
+
+type Rectangle struct {
+	l int
+	b int
+}
+
+func (t *Rectangle) accept(v Visitor) {
+	v.visitForrectangle(t)
+}
+
+func (t *Rectangle) getType() string {
+	return "rectangle"
+}
---
+++ behavioral/visitor/visitor_test.go
@@ -0,0 +1,27 @@
+package visitor
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStrategy(t *testing.T) {
+	t.Parallel()
+	t.Run("", func(t *testing.T) {
+		square := &Square{side: 2}
+		circle := &Circle{radius: 3}
+		rectangle := &Rectangle{l: 2, b: 3}
+
+		areaCalculator := &AreaCalculator{}
+
+		square.accept(areaCalculator)
+		circle.accept(areaCalculator)
+		rectangle.accept(areaCalculator)
+
+		fmt.Println()
+		middleCoordinates := &MiddleCoordinates{}
+		square.accept(middleCoordinates)
+		circle.accept(middleCoordinates)
+		rectangle.accept(middleCoordinates)
+	})
+}
